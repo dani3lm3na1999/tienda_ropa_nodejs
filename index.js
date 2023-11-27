19edf3805e578979930d088394a5247af322d8ff
dani3lm3na1999 <100393140+dani3lm3na1999@users.noreply.github.com>
2023-11-27 00:21:17 -0600

    Cambiando los cors v.2

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(express.static('public'));
 
 // Configuraciones del servido
 app.use(logger('dev'));
-app.use(cors({ origin: 'http://localhost:4200', credentials: true }));
+app.use(cors());
 app.use(helmet());
 
 // Middlewares
