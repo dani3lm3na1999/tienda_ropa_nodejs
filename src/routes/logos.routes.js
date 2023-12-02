23e80117646efe086b05358eb15acdd4b7097cc8
Spunicher <marlonjon1234@gmail.com>
2023-12-02 04:42:20 -0300

    logo por base64

--- src/routes/logos.routes.js
+++ src/routes/logos.routes.js
@@ -3,7 +3,7 @@ const logosController = require('../controllers/logos.controllers');
 const router = express.Router();
 
 router.get('/logos', logosController.obtenerLogos);
-router.post('/logos', logosController.fileUpload, logosController.crearLogo);
+router.post('/logos', logosController.crearLogo);
 router.get('/logos/:tipo', logosController.obtenerLogoFiltrados);
 router.get('/logosid/:id', logosController.obtenerLogoFiltradosPorId);
 
