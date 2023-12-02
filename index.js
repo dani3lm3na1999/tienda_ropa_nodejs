612c3f3738dfa22bf0f47263a4c1d7cdbfa08bd5
Spunicher <marlonjon1234@gmail.com>
2023-12-02 05:11:37 -0300

    cors

--- index.js
+++ index.js
@@ -30,18 +30,19 @@ app.use(express.static('public'));
 
 // Configuraciones del servido
 app.use(logger('dev'));
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+}));
 app.use(helmet());
 
 // Middlewares
 app.use('/api', categoriaRoutes);
 app.use('/api', productosRoutes);
 app.use('/api', logosRoutes);
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+
 // Leer el localhost y variables de entorno
 const port = process.env.PORT || 9000;
 const host = process.env.HOST || '0.0.0.0';
