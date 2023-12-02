6ca54a82e6380d16c2971ee61470916760274151
Spunicher <marlonjon1234@gmail.com>
2023-12-02 04:41:08 -0300

    logo por base64

--- src/controllers/logos.controllers.js
+++ src/controllers/logos.controllers.js
@@ -1,31 +1,10 @@
 const logosModel = require('../models/logos.models');
-const multerConfig = require('../utils/multerConfig.utils')
-const multer = require('multer')
-
-const uploads = multer(multerConfig).array('url', 1);
-
-exports.fileUpload = (req, res, next) => {
-    uploads(req, res, function(error){
-        if (error){
-            res.json({message: error});
-        }
-        return next();
-    });
-};
 
 exports.crearLogo = async (req, res) => {
     try {
         var logoDTO = req.body;
-
-        var urlImagen = `/uploads/${req.files[0].filename}`;
-
-        var logo = new logosModel({
-            url: urlImagen,
-            tipo: logoDTO.tipo
-        });
-
+        var logo = new logosModel(logoDTO)
         await logo.save();
-
         return res.status(200).send(logo);
     } catch (error) {
         return res.status(500).send('Ocurrio un error' + error);
@@ -37,7 +16,6 @@ exports.obtenerLogos = async (req, res) => {
         //
         var logos;
         logos = await logosModel.find();
-
         return res.status(200).send(logos);
     } catch (error) {
         return res.status(500).send('Ocurrio un error' + error);
@@ -48,7 +26,7 @@ exports.obtenerLogoFiltrados = async (req, res) => {
     try {
         var tipologo = req.params.tipo;
         var logos;
-        logos = await logosModel.find({tipo: tipologo});
+        logos = await logosModel.find({ tipo: tipologo });
 
         return res.status(200).send(logos);
     } catch (error) {
